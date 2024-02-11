d67d80ec40c2dd479f1b5958d90d0c36f1491739
justNotherbot <91804283+justNotherbot@users.noreply.github.com>
2024-02-11 22:51:29 +0300

    Update main

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import subprocess
 import os
 import random
+import time
 import main_helpers
 
 
@@ -14,6 +15,9 @@ ERR_STATS_NOT_FOUND = 1
 ERR_STATS_CORRUPT = 2
 N_STATS_WORDS_IN_LINE = 7
 N_TASKS_PER_TEST = 15
+
+WRONG_CMD_MSG = "Неверный формат команды"
+INVALID_LOGIN_MSG = "Невозможно сменить пользователя. Используйте logout."
 
 
 class StatisticsManager:
@@ -74,6 +78,7 @@ class TaskGenerator:
         self.stats_mngr = stats_mngr
         self.task_types = task_types
 
+        self.time_start = 0
         self.u_id = ""
         self.tasks = []
         self.user_answers = []
@@ -97,11 +102,11 @@ class TaskGenerator:
         except Exception:
             return 1
         s_out = out.decode("utf-8").split("<task>")
-        if len(s_out) - 1 != n_total * 2:
+        if len(s_out) - 1 != n_total * 3:
             return 1
                 
-        for i in range(0, len(s_out) - 1, 2):
-            tmp = (s_out[i], s_out[i+1])
+        for i in range(0, len(s_out) - 1, 3):
+            tmp = (s_out[i+1], s_out[i+2], s_out[i])
             self.tasks.append(tmp)
         
         random.shuffle(self.tasks)
@@ -111,6 +116,8 @@ class TaskGenerator:
         for i in range(len(self.tasks)):
             print(str(i+1)+".")
             print(self.tasks[i][0])
+        
+        self.time_start = time.time()
 
     def add_user_answer(self, task_num, ans):
         if 0 <= task_num < len(self.user_answers):
@@ -118,7 +125,28 @@ class TaskGenerator:
 
     def submit(self):
         n_correct = 0
-        
+        stats_per_type = {"B": B_SOLVED_IDX, "D": D_SOLVED_IDX, "E": E_SOLVED_IDX}
+
+        out = []
+
+        for i in range(len(self.user_answers)):
+            is_correct = self.tasks[i][1] == self.user_answers[i]
+            if not is_correct:
+                out.append("Задание " + str(i+1) + f": Ваш ответ: {self.user_answers[i]}. \
+                           Правильный ответ: {self.tasks[i][1]}")
+            n_correct += is_correct
+
+            if self.u_id in self.stats_mngr.stats_by_user:
+                task_type = self.tasks[i][2]
+                idx = stats_per_type[task_type]
+
+                self.stats_mngr.stats_by_user[self.u_id][idx] += is_correct
+                self.stats_mngr.stats_by_user[self.u_id][idx+1] += 1
+
+        curr_time = time.time()
+        print(f"Решено {n_correct} из {len(self.user_answers)}.")
+        print(f"Потрачено {round((curr_time - self.time_start) / 60)} минут.")
+        print(*out)
 
     
     def get_task_distribution(self, n_total):
@@ -126,7 +154,7 @@ class TaskGenerator:
         task_amnt = []
         c_sum = 0
 
-        if self.u_id in self.stats_mngr.stats_by_user:
+        if self.u_id in self.stats_mngr.stats_by_user and not (0 in self.stats_mngr.stats_by_user[self.u_id]):
             ls = self.stats_mngr.stats_by_user[self.u_id]
             initial = 1 - (ls[0] / ls[1])
             for i in range(0, len(ls), 2):
@@ -152,13 +180,114 @@ class TaskGenerator:
         return s
 
 
-stats_manager = StatisticsManager("stats.txt")
-print(stats_manager.err_code, stats_manager.stats_by_user)
-stats_manager.save_stats()
-curr_dir = os.path.dirname(os.path.realpath(__file__))  # Directory where this .py file is located
-tgt_dir = os.path.join(curr_dir, "generator", "generator_main.py")
-task_gen = TaskGenerator(tgt_dir, stats_manager, ["B", "D", "E"])
-task_gen.u_id = "cd"
-task_gen.generate_tasks(N_TASKS_PER_TEST)
-#task_gen.show_tasks()
-#child = subprocess.Popen(['python', 'simple.py'], stdin=subprocess.PIPE)
+class ConsoleContext:
+
+    def __init__(self, script_path, stats_path, task_types):
+        self.u_id = ""
+        self.stats_manager = StatisticsManager(stats_path)
+        self.task_gen = TaskGenerator(script_path, self.stats_manager, task_types)
+
+    def setuid(self, u_id):
+        if self.u_id == "" or u_id == "":
+            self.u_id = u_id
+            self.task_gen.u_id = u_id
+            return 0
+        return 1
+
+    def createuid(self, u_id):
+        self.stats_manager.add_user(u_id)
+
+    def start_test(self):
+        self.task_gen.generate_tasks(N_TASKS_PER_TEST)
+        self.task_gen.show_tasks()
+
+    def answer(self, task_num, ans):
+        self.task_gen.add_user_answer(task_num, ans)
+
+    def submit(self):
+        self.task_gen.submit()
+
+
+def login(console_context, args):
+    if len(args) != 1 or args[0] == "":
+        print(WRONG_CMD_MSG)
+        return
+    
+    err = console_context.setuid(args[0])
+    if err:
+        print(INVALID_LOGIN_MSG)
+
+
+def logout(console_context, args):
+    if len(args) != 0:
+        print(WRONG_CMD_MSG)
+        return
+    
+    console_context.setuid("")
+
+
+def create(console_context, args):
+    if len(args) != 1:
+        print(WRONG_CMD_MSG)
+        return
+
+    console_context.createuid(args[0])
+
+
+def start_test(console_context, args):
+    if len(args) != 0:
+        print(WRONG_CMD_MSG)
+        return
+    
+    console_context.start_test()
+
+
+def answer(console_context, args):
+    if len(args) != 2 or not args[0].isnumeric():
+        print(WRONG_CMD_MSG)
+        return
+    
+    console_context.answer(int(args[0]), args[1])
+
+
+def submit(console_context, args):
+    if len(args) != 0:
+        print(WRONG_CMD_MSG)
+        return
+    
+    console_context.submit()
+
+
+def fini(console_context, args):
+    if len(args) != 0:
+        print(WRONG_CMD_MSG)
+        return
+    
+    console_context.stats_manager.save_stats()
+    exit()
+
+
+command_map = {"login": login,
+               "logout": logout,
+               "exit": fini,
+               "create": create,
+               "start_test": start_test,
+               "answer": answer,
+               "submit": submit}
+
+
+if __name__ == "__main__":
+    curr_dir = os.path.dirname(os.path.realpath(__file__))  # Directory where this .py file is located
+    tgt_dir = os.path.join(curr_dir, "generator", "generator_main.py")
+    c_context = ConsoleContext(tgt_dir, "stats.txt", ["B", "D", "E"])
+
+    while True:
+        s = input(c_context.u_id + ">")
+        words = s.strip().split()
+
+        if len(words):
+            cmd = words[0]
+            if cmd not in command_map:
+                print(WRONG_CMD_MSG)
+            else:
+                command_map[cmd](c_context, words[1:])
